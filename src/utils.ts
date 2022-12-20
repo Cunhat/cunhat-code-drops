00023f88abf4a4ce0d0dcaf7fdd936b0de134292
Tiago Miguel Cunha <tiago92cunha@gmail.com>
2022-12-20 16:31:55 +0000

    First commit with the basic structure

---
+++ src/utils.ts
@@ -0,0 +1,71 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+export const getDropsData = () => {
+    let dir:Directories = new Map();
+    
+    const files = fs.readdirSync('./src/pages', { withFileTypes: true });
+
+    // console.log(files[1].isDirectory());
+
+    files.forEach((file) => {
+        if (file.isDirectory()) {
+            const filesInDir = fs.readdirSync(`./src/pages/${file.name}`);
+            const filesInDirName = filesInDir.filter((file) => file.endsWith(".md"))
+            let filesObg: Array<{name: string}> = [];
+            filesInDirName.forEach((file) => {
+                const obj = {
+                    name: file,
+                }
+                filesObg.push(obj);
+            });
+            // console.log(filesInDirName);
+            dir.set(file.name, {
+                files: filesObg
+            });
+
+            
+        }
+        //console.log(dir);
+    });
+
+    return dir;
+
+
+//     const markdownPosts = files.filter((file) => file.endsWith(".md"));
+
+//   // Get gray-matter data from each file.
+//     const posts = markdownPosts.map((fileName) => {
+//     const fileContents = fs.readFileSync(`pages/${fileName}`, "utf8");
+//     const matterResult = matter(fileContents);
+//     return {
+//       title: matterResult.data.title,
+//       date: matterResult.data.date,
+//       subtitle: matterResult.data.subtitle,
+//       slug: fileName.replace(".md", ""),
+//     };
+//   });
+
+//   return posts;
+}
+
+// const path = [{
+//     "Title": "dirName",
+//     "files": [{
+//         //       title: matterResult.data.title,
+//         //       date: matterResult.data.date,
+//         //       subtitle: matterResult.data.subtitle,
+//         //       slug: fileName.replace(".md", ""),
+//         //     }]
+// }]
+
+type Directories = Map<string,{
+    files: Array<{
+        name: string,
+        // title: string,
+        // date: string,
+        // subtitle: string,
+        // slug: string,
+    }>,
+}>
