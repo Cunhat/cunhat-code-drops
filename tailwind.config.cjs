9186da28e03ec501b58fa23316885764c4ac8baa
Tiago Miguel Cunha <tiago92cunha@gmail.com>
2022-12-20 17:01:38 +0000

    Make layout responsive

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,9 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
-				medium: "JostMedium",
-				regular: "JostRegular",
+				medium: "ChubboMedium",
+				regular: "ChubboRegular",
+				bold: "ChubboBold",
 			  },
 		},
 	},
