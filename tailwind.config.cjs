9c821d1df3839d2e268cc9240ed72e30f80afe3c
Tiago Miguel Cunha <tiago.miguel.cunha@celfocus.com>
2023-01-16 14:47:27 +0000

    Add individual scroll bars by section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				medium: "ChubboMedium",
-				regular: "ChubboRegular",
-				bold: "ChubboBold",
-			  },
-		},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        medium: 'ChubboMedium',
+        regular: 'ChubboRegular',
+        bold: 'ChubboBold',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography'), require('tailwind-scrollbar')],
+};
